e7b25607d80b05e463aa2747db44a967e79a3f16
wahid hasan <wahid.baba22@gmail.com>
2021-04-08 18:23:10 -0400

    any

---
+++ Maven-2020btc/src/main/java/utility/Source.java
@@ -0,0 +1,8 @@
+package utility;
+
+public class Source {
+
+	public void m1() {
+		System.out.println("hi");
+	}
+}
---
+++ Maven-2020btc/src/test/java/com/SmokeTest.java
@@ -0,0 +1,14 @@
+package com;
+
+import org.junit.Test;
+
+import utility.Source;
+
+public class SmokeTest {
+
+	@Test
+	public void test() {
+		Source s=new Source();
+		s.m1();
+	}
+}
